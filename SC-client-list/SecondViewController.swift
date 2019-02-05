396fb5939f075ea7c78533d391339c6fa6ee4266
The Clout <Theclout@macbook-pro-3.local>
2019-02-05 12:11:06 -0600

    Initial Commit

---
+++ SC-client-list/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SC-client-list
+//
+//  Created by The Clout on 2/5/19.
+//  Copyright © 2019 Draper. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SC-client-listTests/SC_client_listTests.swift
@@ -0,0 +1,34 @@
+//
+//  SC_client_listTests.swift
+//  SC-client-listTests
+//
+//  Created by The Clout on 2/5/19.
+//  Copyright © 2019 Draper. All rights reserved.
+//
+
+import XCTest
+@testable import SC_client_list
+
+class SC_client_listTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
