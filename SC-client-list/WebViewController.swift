3e28467e5e0d118d0ecf341eeb2db67de3a5e810
The Clout <Theclout@MacBook-Pro-3.local>
2019-02-05 13:34:36 -0600

    added table and search items to web view

--- SC-client-list/WebViewController.swift
+++ SC-client-list/WebViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class WebViewController: UIViewController {
+class WebTableCell: UITableViewCell{
+    
+}
+
+class WebViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var webClientTable: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
 
