b543841c0c9b9a6d0de7dc9c11e1929e164f041e
The Clout <Theclout@MacBook-Pro-3.local>
2019-02-06 12:31:51 -0600

    Added client fields for the 'add client' view.

--- SC-client-list/WebViewController.swift
+++ SC-client-list/WebViewController.swift
@@ -23,7 +23,7 @@ class WebViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
