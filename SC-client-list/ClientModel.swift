3ccbe23f9b1ea6e91398176a1b52a9507f8993f0
The Clout <Theclout@imac.attlocal.net>
2019-02-09 18:20:36 -0600

    continued work on ClientModel

--- SC-client-list/ClientModel.swift
+++ SC-client-list/ClientModel.swift
@@ -11,17 +11,33 @@ import UIKit
 
 struct Client {
     
-    let clientType: String
-    let firstName: String
-    let lastName: String
-    let clientPhoto: UIImage
+    let clientType: String?
+    let firstName: String?
+    let lastName: String?
+    let clientPhoto: UIImage?
     
-    let companyName: String
-    let companyDesc: String
+    let companyName: String?
+    let companyDesc: String?
     
-    let officePhone: String
-    let cellPhone: String
-    let email: String
-    let website: String
+    let officePhone: Int?
+    let cellPhone: Int?
+    let email: String?
+    let website: String?
+    
+    init(clientType: String? = nil, firstName: String? = nil, lastName: String? = nil, clientPhoto: UIimage = nil, companyName: String? =  nil, companyDesc: String? = nil, officePhone: Int? = nil, cellPhone: Int? = nil, email: String? = nil, website: String? = nil ){
+        
+        self.clientType = clientType
+        self.firstName = firstName
+        self.lastName = lastName
+        self.clientPhoto = clientPhoto
+        
+        self.companyName = companyName
+        self.companyDesc = companyDesc
+        
+        self.officePhone = officePhone
+        self.cellPhone = cellPhone
+        self.email = email
+        self.website = website
+    }
     
 }
